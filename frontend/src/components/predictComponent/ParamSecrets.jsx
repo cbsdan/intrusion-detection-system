1930bf97b1c57a62ea4d193ec1df9180fdf12e94
Daniel Cabasa <cabasadaniel1@gmail.com>
2024-11-17 08:13:54 +0800

    modify analysis result page

--- frontend/src/components/predictComponent/ParamSecrets.jsx
+++ frontend/src/components/predictComponent/ParamSecrets.jsx
@@ -8,12 +8,43 @@ const ParamSecrets = () => {
   console.log(data);
   let navigate = useNavigate();
 
+  const capitalize = (str) => {
+    return str
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      .join(" ");
+  };
+  
+
+  const extractInfo = (data) => {
+    const extractBinaryClass = (bin_cls) => bin_cls.split(":")[1].trim();
+    const extractMultiClass = (mul_cls) => mul_cls.split(":")[1].trim();
+    const extractMultiClassDesc = (description) =>
+      description.split(":")[1].trim();
+
+    const result = {};
+
+    for (const model in data) {
+      if (data.hasOwnProperty(model)) {
+        result[model] = {
+          bin_cls: extractBinaryClass(data[model].bin_cls),
+          mul_cls: capitalize(extractMultiClass(data[model].mul_cls)),
+          description: extractMultiClassDesc(data[model].description),
+        };
+      }
+    }
+
+    return result;
+  };
+
   // Redirect if no resultData
   useEffect(() => {
     if (!resultData) {
       navigate("/predict/parameters");
     } else {
-      setData(resultData);
+      console.log(resultData);
+      const extractedData = extractInfo(resultData);
+      setData(extractedData);
     }
   }, [resultData, navigate]);
 
@@ -29,23 +60,62 @@ const ParamSecrets = () => {
       >
         {/* KNN Card */}
         <div
-          className="card text-center"
+          className="card"
           style={{
             width: "35rem",
             margin: "0px 30px 30px 30px",
             backgroundColor: "#71C9CE",
           }}
         >
-          <div className="card-body">
-            <h4 className="card-title">K-Nearest-Neighbor (KNN)</h4>
-            <p className="card-text">{data.knn.bin_cls}</p>
-            <p className="card-text">{data.knn.mul_cls}</p>
-            <p className="card-text">{data.knn.description}</p>
-            <p className="card-text">
-              KNN Binary Class Accuracy = 0.9760368900303525
+          <div className="card-body d-flex align-items-start flex-column">
+            <h4 className="card-title w-100 text-center">
+              K-Nearest-Neighbor (KNN)
+            </h4>
+            <div className=" mt-2">
+              <h5>Binary Class Result</h5>
+              <h3
+                className={
+                  data.knn.bin_cls == "Attack"
+                    ? "text-danger d-flex align-items-center justify-content-center"
+                    : "text-primary"
+                }
+              >
+                {data.knn.bin_cls == "Attack" ? (
+                  <i class="fa-solid fa-triangle-exclamation"></i>
+                ) : (
+                  <i class="fa-solid fa-shield"></i>
+                )}
+                <span className="px-2">{data.knn.bin_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2">
+              <h5>Multi Class Result</h5>
+              <h3
+                className={
+                  data.knn.mul_cls != "Normal" ? "text-warning" : "text-primary"
+                }
+              >
+                {data.knn.mul_cls == "Normal" ? (
+                  <i class="fa-solid fa-check"></i>
+                ) : (
+                  <i class="fa-solid fa-flag-checkered"></i>
+                )}
+                <span className="px-2">{data.knn.mul_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2" style={{ textAlign: "left" }}>
+              <h5>Description</h5>
+              <p className="text-secondary">
+                &nbsp;&nbsp;&nbsp;&nbsp;{data.knn.description}
+              </p>
+            </div>
+            <p className="card-text mt-2 mb-1">
+              <span className="text-white">Binary Class Accuracy</span> ={" "}
+              <span>0.9760368900303525</span>
             </p>
             <p className="card-text">
-              KNN Multi Class Accuracy = 0.9740368900303525
+              <span className="text-white">Multi Class Accuracy</span> =
+              <span>0.9740368900303525</span>
             </p>
             <Link
               className="btn btn-success"
@@ -68,28 +138,65 @@ const ParamSecrets = () => {
 
         {/* Random Forest Card */}
         <div
-          className="card text-center"
+          className="card"
           style={{
             width: "35rem",
             margin: "0px 30px 30px 30px",
             backgroundColor: "#71C9CE",
           }}
         >
-          <div className="card-body">
-            <h4 className="card-title">Random Forest</h4>
-            <p className="card-text">{data.rf.bin_cls}</p>
-            <p className="card-text">{data.rf.mul_cls}</p>
-            <p className="card-text">{data.rf.description}</p>
-            <p className="card-text">
-              Random Forest Binary Class Accuracy = 0.9741029652113005
+          <div className="card-body d-flex align-items-start flex-column">
+            <h4 className="card-title w-100 text-center">Random Forest</h4>
+            <div className=" mt-2">
+              <h5>Binary Class Result</h5>
+              <h3
+                className={
+                  data.rf.bin_cls == "Attack"
+                    ? "text-danger d-flex align-items-center justify-content-center"
+                    : "text-primary"
+                }
+              >
+                {data.rf.bin_cls == "Attack" ? (
+                  <i class="fa-solid fa-triangle-exclamation"></i>
+                ) : (
+                  <i class="fa-solid fa-shield"></i>
+                )}
+                <span className="px-2">{data.rf.bin_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2">
+              <h5>Multi Class Result</h5>
+              <h3
+                className={
+                  data.rf.mul_cls != "Normal" ? "text-warning" : "text-primary"
+                }
+              >
+                {data.rf.mul_cls == "Normal" ? (
+                  <i class="fa-solid fa-check"></i>
+                ) : (
+                  <i class="fa-solid fa-flag-checkered"></i>
+                )}
+                <span className="px-2">{data.rf.mul_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2" style={{ textAlign: "left" }}>
+              <h5>Description</h5>
+              <p className="text-secondary">
+                &nbsp;&nbsp;&nbsp;&nbsp;{data.rf.description}
+              </p>
+            </div>
+            <p className="card-text mt-2">
+              <span className="text-white">Binary Class Accuracy</span> =
+              <span>0.9760368900303525</span>
             </p>
             <p className="card-text">
-              Random Forest Multi Class Accuracy = 0.9731029652113005
+              <span className="text-white">Multi Class Accuracy</span> =
+              <span>0.9740368900303525</span>
             </p>
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/rf_bin_table"
+              to="/knn_bin_table"
               role="button"
             >
               Binary-Class Classification Table
@@ -97,7 +204,7 @@ const ParamSecrets = () => {
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/rf_table"
+              to="/knn_table"
               role="button"
             >
               Multi-Class Classification Table
@@ -107,28 +214,67 @@ const ParamSecrets = () => {
 
         {/* CNN Card */}
         <div
-          className="card text-center"
+          className="card"
           style={{
             width: "35rem",
             margin: "0px 30px 30px 30px",
             backgroundColor: "#71C9CE",
           }}
         >
-          <div className="card-body">
-            <h4 className="card-title">Convolutional Neural Network (CNN)</h4>
-            <p className="card-text">{data.cnn.bin_cls}</p>
-            <p className="card-text">{data.cnn.mul_cls}</p>
-            <p className="card-text">{data.cnn.description}</p>
-            <p className="card-text">
-              CNN Binary Class Accuracy = 0.9582535605883726
+          <div className="card-body d-flex align-items-start flex-column">
+            <h4 className="card-title w-100 text-center">
+              Convolutional Neural Network (CNN)
+            </h4>
+            <div className=" mt-2">
+              <h5>Binary Class Result</h5>
+              <h3
+                className={
+                  data.cnn.bin_cls == "Attack"
+                    ? "text-danger d-flex align-items-center justify-content-center"
+                    : "text-primary"
+                }
+              >
+                {data.cnn.bin_cls == "Attack" ? (
+                  <i class="fa-solid fa-triangle-exclamation"></i>
+                ) : (
+                  <i class="fa-solid fa-shield"></i>
+                )}
+                <span className="px-2">{data.cnn.bin_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2">
+              <h5>Multi Class Result</h5>
+              <h3
+                className={
+                  data.cnn.mul_cls != "Normal" ? "text-warning" : "text-primary"
+                }
+              >
+                {data.cnn.mul_cls == "Normal" ? (
+                  <i class="fa-solid fa-check"></i>
+                ) : (
+                  <i class="fa-solid fa-flag-checkered"></i>
+                )}
+                <span className="px-2">{data.cnn.mul_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2" style={{ textAlign: "left" }}>
+              <h5>Description</h5>
+              <p className="text-secondary">
+                &nbsp;&nbsp;&nbsp;&nbsp;{data.cnn.description}
+              </p>
+            </div>
+            <p className="card-text mt-2">
+              <span className="text-white">Binary Class Accuracy</span> =
+              <span>0.9760368900303525</span>
             </p>
             <p className="card-text">
-              CNN Multi Class Accuracy = 0.9506420733130982
+              <span className="text-white">Multi Class Accuracy</span> =
+              <span>0.9740368900303525</span>
             </p>
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/cnn_bin_table"
+              to="/knn_bin_table"
               role="button"
             >
               Binary-Class Classification Table
@@ -136,7 +282,7 @@ const ParamSecrets = () => {
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/cnn_table"
+              to="/knn_table"
               role="button"
             >
               Multi-Class Classification Table
@@ -146,28 +292,69 @@ const ParamSecrets = () => {
 
         {/* LSTM Card */}
         <div
-          className="card text-center"
+          className="card"
           style={{
             width: "35rem",
             margin: "0px 30px 30px 30px",
             backgroundColor: "#71C9CE",
           }}
         >
-          <div className="card-body">
-            <h4 className="card-title">Long Short Term Memory (LSTM)</h4>
-            <p className="card-text">{data.lstm.bin_cls}</p>
-            <p className="card-text">{data.lstm.mul_cls}</p>
-            <p className="card-text">{data.lstm.description}</p>
-            <p className="card-text">
-              LSTM Binary Class Accuracy = 0.9562456222274107
+          <div className="card-body d-flex align-items-start flex-column">
+            <h4 className="card-title w-100 text-center">
+              Long Short Term Memory (LSTM){" "}
+            </h4>
+            <div className=" mt-2">
+              <h5>Binary Class Result</h5>
+              <h3
+                className={
+                  data.lstm.bin_cls == "Attack"
+                    ? "text-danger d-flex align-items-center justify-content-center"
+                    : "text-primary"
+                }
+              >
+                {data.lstm.bin_cls == "Attack" ? (
+                  <i class="fa-solid fa-triangle-exclamation"></i>
+                ) : (
+                  <i class="fa-solid fa-shield"></i>
+                )}
+                <span className="px-2">{data.lstm.bin_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2">
+              <h5>Multi Class Result</h5>
+              <h3
+                className={
+                  data.lstm.mul_cls != "Normal"
+                    ? "text-warning"
+                    : "text-primary"
+                }
+              >
+                {data.lstm.mul_cls == "Normal" ? (
+                  <i class="fa-solid fa-check"></i>
+                ) : (
+                  <i class="fa-solid fa-flag-checkered"></i>
+                )}
+                <span className="px-2">{data.lstm.mul_cls}</span>
+              </h3>
+            </div>
+            <div className="card-text mt-2" style={{ textAlign: "left" }}>
+              <h5>Description</h5>
+              <p className="text-secondary">
+                &nbsp;&nbsp;&nbsp;&nbsp;{data.lstm.description}
+              </p>
+            </div>
+            <p className="card-text mt-2">
+              <span className="text-white">Binary Class Accuracy</span> =
+              <span>0.9760368900303525</span>
             </p>
             <p className="card-text">
-              LSTM Multi Class Accuracy = 0.9590940929255195
+              <span className="text-white">Multi Class Accuracy</span> =
+              <span>0.9740368900303525</span>
             </p>
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/lstm_bin_table"
+              to="/knn_bin_table"
               role="button"
             >
               Binary-Class Classification Table
@@ -175,7 +362,7 @@ const ParamSecrets = () => {
             <Link
               className="btn btn-success"
               style={{ textAlign: "center", backgroundColor: "#5bc0de" }}
-              to="/lstm_table"
+              to="/knn_table"
               role="button"
             >
               Multi-Class Classification Table
